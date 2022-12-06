967128b702b1e311ec9f82376329b36b8ba850f3
Batuhan Eyüboğlu <batuhan.eyuboglu@outlook.com>
2022-12-07 01:20:07 +0300

    JLabel example

--- src/GUIManager.java
+++ src/GUIManager.java
@@ -1,16 +1,36 @@
 import java.awt.Color;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class GUIManager {
 	GUIManager(){
+		
+		JLabel label = CreateLabel("DENEMEEEEEEEE", null, JLabel.TOP);				
+		
+		JFrame frame =  CreateFrame();
+		frame.add(label);
+	}
+
+
+	public JFrame CreateFrame() {
 		JFrame frame = new JFrame(); // Create a frame
 		frame.setTitle("Title"); // Frame title
 		frame. setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
 		frame.setResizable(false); //prevent frame from being resized
-		frame.setSize(420,420); //sets the x-dimension, and y-dimension of frame
+		frame.setSize(800,800); //sets the x-dimension, and y-dimension of frame
 		frame.setVisible(true); //make frame visible
 
-		frame. getContentPane().setBackground(Color.white); //change color of background
+		frame. getContentPane().setBackground(new Color(100,100,100)); //change color of background
+		return frame;
+	}
+	
+	
+	public JLabel CreateLabel(String text, Integer horizontalPosition, Integer verticalPosition) {
+		JLabel label = new JLabel();
+		label.setText(text);
+		if(horizontalPosition != null)label.setHorizontalAlignment(horizontalPosition);
+		if(verticalPosition != null)label.setVerticalAlignment(verticalPosition);
+		return label;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 import java.io.IOException;
+
 import java.net.URL;
 import java.util.Scanner;
 
 import javax.swing.JFrame;
+
 public class Main {
    public static void main(String args[]) {
-	   GUIManager guiManager = new GUIManager();
+	   Init();
+   }
+   
+   public static void Init() {
+	   GUIManager guiManager = new GUIManager();	
    }
    
 }
